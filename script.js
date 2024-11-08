59372e82ca10b52ea33d74b8b5e8a424f2d2569c
axelquartz <axelarep@gmail.com>
2024-11-08 15:18:29 -0600

    The program works as intended, no extra functionality nor styles added

--- script.js
+++ script.js
@@ -1,16 +1,43 @@
 // Create individual square
-let newSquare = document.createElement("div");
-newSquare.classList.add("square");
-newSquare.style.backgroundColor = "yellow";
+let container = document.querySelector(".container");
+let squareNumber = document.querySelector("#square-number");
+let squareGenerationButton = document.querySelector("#square-generation-button");
 
-// Change color of square on mouseover
-newSquare.addEventListener("mouseover", function () {
-  if (newSquare.style.backgroundColor === "yellow") {
-    newSquare.style.backgroundColor = "black";
+// Define square properties
+// newSquare.classList.add("square");
+// newSquare.style.backgroundColor = "yellow";
+
+// Create squares dinamically
+squareGenerationButton.addEventListener("click", function () {
+  if (squareNumber.value < 1 || squareNumber.value > 100) {
+    alert("Please enter a number between 1 and 100");
   } else {
-    newSquare.style.backgroundColor = "yellow";
+    for (let i = 0; i < squareNumber.value * squareNumber.value; i++) {
+      container.style.width = `${Number(squareNumber.value) * 16}px`;
+      container.style.height = `${Number(squareNumber.value) * 16}px`;
+      console.log(Number(squareNumber.value));
+
+      let newSquare = document.createElement("div");
+      newSquare.classList.add("square");
+      newSquare.style.backgroundColor = "yellow";
+      container.appendChild(newSquare);
+
+      newSquare.addEventListener("mouseover", function () {
+        if (newSquare.style.backgroundColor === "yellow") {
+          newSquare.style.backgroundColor = "black";
+        } else {
+          newSquare.style.backgroundColor = "yellow";
+        }
+      });
+    }
   }
 });
 
-let container = document.querySelector(".container");
-container.appendChild(newSquare);
+// Change color of square on mouseover
+// squareSelected.addEventListener("mouseover", function () {
+//   if (newSquare.style.backgroundColor === "yellow") {
+//     newSquare.style.backgroundColor = "black";
+//   } else {
+//     newSquare.style.backgroundColor = "yellow";
+//   }
+// });
